1c6011ed2d21c1b933e8c8949b2334c83da7a266
misusuario <micorreo@gmail.com>
2021-01-26 16:54:54 +0100

    prueba

---
+++ js/3d-controles.js
@@ -0,0 +1,55 @@
+//OJO, només podem tenir un dem activat al mapa (deixar l'altre comentat)
+var fuente;
+
+function addICGC (){
+    var fuente = 'icgc-dem';
+}
+
+function addMB (){
+    var fuente = 'mapbox-dem';
+}
+
+function donothing (){
+    var fuente = 'none';
+}
+
+function add3D() {
+
+    map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+    });
+
+    map.addSource('icgc-dem', {
+        'type': 'raster-dem',
+        "tiles": [
+            "https://tilemaps.icgc.cat/tileserver/tileserver.php/terreny_icgc_2m_rgb/{z}/{x}/{y}.png"
+        ],
+        'tileSize': 512,
+        'maxzoom': 14
+    });
+
+    map.setTerrain({
+        'source': 'fuente',
+        'exaggeration': 1.5 //1 altura real. + de 1 exageracion.
+    });
+
+    map.addLayer({ //degradado azul de fondo para que parezca + real 
+        'id': 'sky',
+        'type': 'sky',
+        'paint': {
+            'sky-type': 'atmosphere',
+            'sky-atmosphere-sun': [0.0, 0.0],
+            'sky-atmosphere-sun-intensity': 15
+        }
+    });
+
+} //fin funcion add3D
+
+
+
+
+
+
